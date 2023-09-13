0fd3b36a486abe7deb3befc65a6a824cc6f47729
Isaac Mogrovejo <isaac.mogrovejo.contractor@bbva.com>
2023-09-13 16:11:28 -0500

    PERAR-24749: migración de entidades

---
+++ src/main/java/com/bbva/pisd/dto/insurancedao/operation/OperationConstants.java
@@ -0,0 +1,12 @@
+package com.bbva.pisd.dto.insurancedao.operation;
+
+public class OperationConstants {
+
+    public final class Operation{
+        public static final String UPDATE = "UPDATE";
+        public static final String SELECT = "SELECT";
+        public static final String BATCH = "BATCH";
+
+        private Operation() {}
+    }
+}
