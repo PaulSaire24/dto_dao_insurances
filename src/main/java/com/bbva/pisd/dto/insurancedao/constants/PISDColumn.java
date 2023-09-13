78739a42d9576e000d75306ec499d2327b6cde2d
Isaac Mogrovejo <isaac.mogrovejo.contractor@bbva.com>
2023-09-13 17:34:13 -0500

    PERAR-24749: se agrega PISDColumn

---
+++ src/main/java/com/bbva/pisd/dto/insurancedao/constants/PISDColumn.java
@@ -0,0 +1,49 @@
+package com.bbva.pisd.dto.insurancedao.constants;
+
+public class PISDColumn {
+
+    public final class Contract{
+        public static final String FIELD_INSURANCE_CONTRACT_ENTITY_ID = "INSURANCE_CONTRACT_ENTITY_ID";
+        public static final String FIELD_INSURANCE_CONTRACT_BRANCH_ID = "INSURANCE_CONTRACT_BRANCH_ID";
+        public static final String FIELD_INSRC_CONTRACT_INT_ACCOUNT_ID = "INSRC_CONTRACT_INT_ACCOUNT_ID";
+        public static final String FIELD_CONTRACT_FIRST_VERFN_DIGIT_ID = "CONTRACT_FIRST_VERFN_DIGIT_ID";
+        public static final String FIELD_CONTRACT_SECOND_VERFN_DIGIT_ID = "CONTRACT_SECOND_VERFN_DIGIT_ID";
+        public static final String FIELD_PREMIUM_AMOUNT = "PREMIUM_AMOUNT";
+        public static final String FIELD_INSURANCE_MODALITY_TYPE = "INSURANCE_MODALITY_TYPE";
+        public static final String FIELD_INSURANCE_COMPANY_PRODUCT_ID = "INSURANCE_COMPANY_PRODUCT_ID";
+        public static final String FIELD_INSURANCE_PRODUCT_TYPE = "INSURANCE_PRODUCT_TYPE";
+        public static final String FIELD_INSURANCE_PRODUCT_ID = "INSURANCE_PRODUCT_ID";
+        public static final String FIELD_POLICY_ID = "POLICY_ID";
+        public static final String FIELD_INSURANCE_COMPANY_CONTRACT_ID = "INSURANCE_COMPANY_CONTRACT_ID";
+        public static final String FIELD_SETTLEMENT_FIX_PREMIUM_AMOUNT = "SETTLEMENT_FIX_PREMIUM_AMOUNT";
+        public static final String FIELD_SETTLE_PENDING_PREMIUM_AMOUNT = "SETTLE_PENDING_PREMIUM_AMOUNT";
+        public static final String FIELD_CTRCT_DISPUTE_STATUS_TYPE = "CTRCT_DISPUTE_STATUS_TYPE";
+        public static final String FIELD_USER_AUDIT_ID = "USER_AUDIT_ID";
+        public static final String FIELD_CONTRACT_STATUS_ID = "CONTRACT_STATUS_ID";
+        public static final String FIELD_CURRENCY_ID = "CURRENCY_ID";
+        public static final String FIELD_INSURANCE_CONTRACT_START_DATE = "INSURANCE_CONTRACT_START_DATE";
+        public static final String FIELD_INSURED_AMOUNT = "INSURED_AMOUNT";
+        public static final String FIELD_INSURANCE_CONTRACT_END_DATE = "INSURANCE_CONTRACT_END_DATE";
+        public static final String FIELD_CONTRACT_RENEWAL_STATUS_TYPE = "CONTRACT_RENEWAL_STATUS_TYPE";
+        public static final String FIELD_RENEWAL_NUMBER = "RENEWAL_NUMBER";
+        public static final String FIELD_CUSTOMER_ID = "CUSTOMER_ID";
+        public static final String FIELD_PAYMENT_FREQUENCY_ID = "PAYMENT_FREQUENCY_ID";
+        public static final String FIELD_NON_RENEWAL_CTRCT_REASON_DESC = "NON_RENEWAL_CTRCT_REASON_DESC";
+        public static final String FIELD_CONTRACT_RENEWAL_RECEIPT_DATE = "CONTRACT_RENEWAL_RECEIPT_DATE";
+        public static final String FIELD_INSRC_COMPANY_RENWL_RSPSE_DESC = "INSRC_COMPANY_RENWL_RSPSE_DESC";
+        public static final String FIELD_PAYMENT_MEANS_TYPE = "PAYMENT_MEANS_TYPE";
+    }
+
+    public final class Receipt{
+        public static final String FIELD_INSURANCE_CONTRACT_ENTITY_ID = "INSURANCE_CONTRACT_ENTITY_ID";
+        public static final String FIELD_INSURANCE_CONTRACT_BRANCH_ID = "INSURANCE_CONTRACT_BRANCH_ID";
+        public static final String FIELD_INSRC_CONTRACT_INT_ACCOUNT_ID = "INSRC_CONTRACT_INT_ACCOUNT_ID";
+        public static final String FIELD_POLICY_RECEIPT_ID = "POLICY_RECEIPT_ID";
+        public static final String FIELD_INSURANCE_COMPANY_RECEIPT_ID = "INSURANCE_COMPANY_RECEIPT_ID";
+        public static final String FIELD_PREMIUM_PAYMENT_RECEIPT_AMOUNT = "PREMIUM_PAYMENT_RECEIPT_AMOUNT";
+        public static final String FIELD_CURRENCY_ID = "CURRENCY_ID";
+        public static final String FIELD_RECEIPT_STATUS_TYPE = "RECEIPT_STATUS_TYPE";
+
+    }
+
+}
