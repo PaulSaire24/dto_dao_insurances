6a07c2a848d51fe0e0dbec9f795e710b541383b4
Jose Artica <jose.artica@bbva.com>
2023-10-02 20:25:18 -0500

    feat: PERAR-24749 add PISDConstant

---
+++ src/main/java/com/bbva/pisd/dto/insurancedao/constants/PISDConstant.java
@@ -0,0 +1,17 @@
+package com.bbva.pisd.dto.insurancedao.constants;
+
+public class PISDConstant {
+
+    private PISDConstant() {
+    }
+
+    public static final class Pagination {
+        public static final String COLUMN_COUNT = "COUNT";
+        public static final String FROM = "FROM";
+        public static final Integer PAGINATION = 5000;
+    }
+    public static final class Numeros {
+        public static final int CERO = 0;
+
+    }
+}
