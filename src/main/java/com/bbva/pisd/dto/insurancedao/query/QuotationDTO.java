c0ba0fbd77589fe2377fe7776a30b3f4fe7c6c0b
Aaron <carlos.carrillo.delgado@bbva.com>
2024-03-20 17:51:43 -0500

    feat: PERAR-26901 PERAR-27052 Modify name of classes and add constants field name of tables

---
+++ src/main/java/com/bbva/pisd/dto/insurancedao/query/QuotationDTO.java
@@ -0,0 +1,16 @@
+package com.bbva.pisd.dto.insurancedao.query;
+
+
+public class QuotationDTO {
+
+    private QuotationModDTO quotationMod;
+
+
+    public QuotationModDTO getQuotationMod() {
+        return quotationMod;
+    }
+
+    public void setQuotationMod(QuotationModDTO quotationMod) {
+        this.quotationMod = quotationMod;
+    }
+}
