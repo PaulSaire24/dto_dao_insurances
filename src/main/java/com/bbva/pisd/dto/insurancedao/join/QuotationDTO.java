929e7fc7b45d7e09f8125c344b94589b0b48a0fa
Aaron <carlos.carrillo.delgado@bbva.com>
2024-03-20 17:51:43 -0500

    feat: PERAR-26901 PERAR-27052 Modify name of classes and add constants field name of tables

---
+++ src/main/java/com/bbva/pisd/dto/insurancedao/join/QuotationDTO.java
@@ -0,0 +1,16 @@
+package com.bbva.pisd.dto.insurancedao.join;
+
+
+public class QuotationDTO {
+
+    private QuotationModDTO quotationMod;
+
+
+    public QuotationModDTO getQuotationMod() {
+        return quotationMod;
+    }
+
+    public void setQuotationMod(QuotationModDTO quotationMod) {
+        this.quotationMod = quotationMod;
+    }
+}
---
+++ src/main/java/com/bbva/pisd/dto/insurancedao/join/QuotationModDTO.java
@@ -0,0 +1,36 @@
+package com.bbva.pisd.dto.insurancedao.join;
+
+import com.bbva.pisd.dto.insurancedao.entities.ModalityEntity;
+import com.bbva.pisd.dto.insurancedao.entities.QuotationModEntity;
+
+public class QuotationModDTO {
+
+    private QuotationModEntity quotationMod;
+    private ModalityEntity modalityEntity;
+    private String insuranceProductType;
+
+
+    public QuotationModEntity getQuotationMod() {
+        return quotationMod;
+    }
+
+    public void setQuotationMod(QuotationModEntity quotationMod) {
+        this.quotationMod = quotationMod;
+    }
+
+    public ModalityEntity getModalityEntity() {
+        return modalityEntity;
+    }
+
+    public void setModalityEntity(ModalityEntity modalityEntity) {
+        this.modalityEntity = modalityEntity;
+    }
+
+    public String getInsuranceProductType() {
+        return insuranceProductType;
+    }
+
+    public void setInsuranceProductType(String insuranceProductType) {
+        this.insuranceProductType = insuranceProductType;
+    }
+}
